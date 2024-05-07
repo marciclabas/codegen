2668424ff1cdd999a49e9dd7dcdc33a3ab5bb62e
m4rs <marcel@marcelclaramunt.cat>
2024-05-07 15:27:02 +0200

    verify-import: += dslog

--- verify-import/src/verify_import/cli.py
+++ verify-import/src/verify_import/cli.py
@@ -4,18 +4,23 @@ def main():
   parser = argparse.ArgumentParser(description='Import Checker')
   parser.add_argument('PKG', help='Module/package to verify')
   parser.add_argument('-v', '--verbose', action='store_true', help='Verbose output')
-
+  
   args = parser.parse_args()
+
+  from dslog import Logger
   from verify_import import walk_modules
+
+  logger = Logger.rich().prefix('[VERIFY IMPORT]').limit('DEBUG' if args.verbose else 'INFO')
+
   errored = False
   for pkg, e in walk_modules(args.PKG):
     if e is not None:
-      print(f"ERROR importing '{pkg}':", e)
+      logger(f"Error importing '{pkg}':", e, level='ERROR')
       errored = True
-    elif args.verbose:
-      print(f"Imported '{pkg}'")
+    else:
+      logger(f"Imported '{pkg}'", level='DEBUG')
   if not errored:
-    print('No errors found')
+    logger('No errors found')
 
 if __name__ == '__main__':
   main()

27d62577286620694e8851844957219217867407
m4rs <marcel@marcelclaramunt.cat>
2024-07-02 10:58:30 +0200

    openapi-ts upgrade

--- openapi-ts/src/openapi_ts/main.py
+++ openapi-ts/src/openapi_ts/main.py
@@ -4,7 +4,7 @@ from tempfile import mktemp
 import json
 
 def generate_client(
-  openapi_schema: dict, package_path: str, *,
+  openapi_schema: dict, output_path: str, *,
   args: Mapping[str, str] = {
     '--client': '@hey-api/client-fetch',
     '--services': '{ asClass: false }'
@@ -20,19 +20,16 @@ def generate_client(
   with open(file, 'w') as f:
     json.dump(openapi_schema, f)
 
-  os.makedirs(package_path, exist_ok=True)
-  os.chdir(package_path)
-  if not os.path.exists('package.json'):
-    with open('package.json', 'w') as f:
-      f.write('{}')
-
   str_args = ' '.join(f'{k} {v}' for k, v in args.items())
-  os.system(f'npx @hey-api/openapi-ts -i {file} -o src/ {str_args}')
+  cmd = f'npx @hey-api/openapi-ts -i {file} -o {output_path} {str_args}'
+  if logstream:
+    print(f'Running: {cmd}', file=logstream)
+  os.system(cmd)
 
   # replace all ".gen'" with ".gen.js'" inside the generated files
   if logstream:
     print('Fixing generated files for NodeNext imports...', file=logstream)
-  for root, _, files in os.walk('src'):
+  for root, _, files in os.walk(output_path):
     for file in files:
       if file.endswith('.ts'):
         with open(os.path.join(root, file), 'r') as f:
